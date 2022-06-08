0b820d81bded751f4201d048da48650717ff1c92
Antoine Siméon <a.simeon@proxiad.com>
2022-06-08 11:50:04 +0200

    Add password confirmation and improvements in "HealthProfessional" model

--- Tremplin/Models/HealthProfessional.cs
+++ Tremplin/Models/HealthProfessional.cs
@@ -6,16 +6,42 @@ namespace Tremplin.Models
     [Table("HealthProfessional")]
     public class HealthProfessional
     {
+        /// <summary>
+        /// Health professional Id
+        /// </summary>
         [Key, Required]
         public int Id { get; set; }
 
-        [Required]
+        /// <summary>
+        /// Health professional identifier
+        /// </summary>
+        [Required(ErrorMessage = "Le champ {0} est requis")]
+        [Display(Name = "Identifiant")]
         public string UserName { get; set; }
 
-        [Required]
+        /// <summary>
+        /// Health professional password
+        /// </summary>
+        [Required(ErrorMessage = "Le champ {0} est requis")]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        /// <summary>
+        /// Password confirmation for health professionals
+        /// </summary>
+        [Required(ErrorMessage = "Le mot de passe doit être confirmé")]
+        [Display(Name = "Confirmation du mot de passe")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Les mots de passe ne correspondent pas")]
+        [NotMapped]
+        public string ConfirmedPassword { get; set; }
+
+        /// <summary>
+        /// Health professional email
+        /// </summary>
+        [Required(ErrorMessage = "Le champ {0} est requis")]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
     }
 }

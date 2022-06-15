0065902422045f8a3a6f723df0c8039ec47c68e8
Antoine Siméon <a.simeon@proxiad.com>
2022-06-15 16:16:56 +0200

    [Test] Add test for checking if birth date is not after today's date

--- Tremplin.Tests/PatientTest.cs
+++ Tremplin.Tests/PatientTest.cs
@@ -6,6 +6,22 @@ namespace Tremplin.Tests
     public class PatientTest
     {
         /// <summary>
+        /// Test if birth date is not after today's date
+        /// </summary>
+        [TestMethod]
+        public void Compare_BirthDate_NotAfterToday()
+        {
+            // Arrange
+            DateTime birthDate = new DateTime(1995, 04, 30);
+
+            // Act
+            int result = DateTime.Compare(birthDate, DateTime.Today);
+
+            // Assert
+            Assert.IsTrue(result <= 0, "Birth date is after today's date");
+        }
+
+        /// <summary>
         /// Test if blank spaces in social security number are correctly removed
         /// </summary>
         [TestMethod]
@@ -15,10 +31,10 @@ namespace Tremplin.Tests
             string socialSecurityNumber = "1 90 26 35 777 888 55";
 
             // Act
-            string actual = Regex.Replace(socialSecurityNumber, @"\s", "");
+            string result = Regex.Replace(socialSecurityNumber, @"\s", "");
 
             // Assert
-            Assert.AreEqual("190263577788855", actual);
+            Assert.AreEqual("190263577788855", result);
         }
     }
 }

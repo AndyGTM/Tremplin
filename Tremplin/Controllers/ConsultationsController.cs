30ceeb0447584236b399e68cede435ad4f6c78af
Antoine Siméon <a.simeon@proxiad.com>
2022-06-21 14:58:59 +0200

    [Feat] Add view for creating consultations

--- Tremplin/Controllers/ConsultationsController.cs
+++ Tremplin/Controllers/ConsultationsController.cs
@@ -19,19 +19,68 @@ namespace Tremplin.Controllers
         /// <summary>
         /// Provides access to the view for listing consultations
         /// </summary>
+        /// <param name="id">Patient Id</param>
         [HttpGet]
-        public async Task<IActionResult> Index(int patientId)
+        public async Task<IActionResult> Index(int id, ConsultationListViewModel consultationListViewModel)
         {
             IQueryable<Consultation> consultations = from m in DataContext.Consultations
-                                                     where m.PatientId == patientId
+                                                     where m.PatientId == id
                                                      select m;
 
-            ConsultationListViewModel consultationListViewModel = new ConsultationListViewModel
-            {
-                Consultations = await consultations.ToListAsync()
-            };
+            consultationListViewModel.PatientId = id;
+            consultationListViewModel.Consultations = await consultations.ToListAsync();
 
             return View(consultationListViewModel);
+        }
+
+        /// <summary>
+        /// Provides access to the view for creating a consultation
+        /// </summary>
+        /// <param name="id">Patient Id</param>
+        /// <param name="consultationCreationViewModel">Consultation information</param>
+        [HttpGet]
+        public IActionResult Create(int id, ConsultationCreationViewModel consultationCreationViewModel)
+        {
+            consultationCreationViewModel.Id = id;
+
+            return View(consultationCreationViewModel);
+        }
+
+        /// <summary>
+        /// Allows to create a consultation
+        /// </summary>
+        /// <param name="consultationCreationViewModel">Consultation information</param>
+        [HttpPost]
+        public async Task<IActionResult> Create(ConsultationCreationViewModel consultationCreationViewModel)
+        {
+            IActionResult result;
+
+            // Valid input data ?
+            if (!this.ModelState.IsValid)
+            {
+                result = this.View(consultationCreationViewModel);
+            }
+            else
+            {
+                // Consultation creation
+                Consultation consultation = new()
+                {
+                    Date = consultationCreationViewModel.Date,
+                    ShortDescription = consultationCreationViewModel.ShortDescription,
+                    LongDescription = consultationCreationViewModel.LongDescription,
+                    PatientId = consultationCreationViewModel.Id
+                };
+
+                // Adding the consultation to the data context
+                DataContext.Add(consultation);
+
+                // Persistence of adding the consultation to the database
+                await DataContext.SaveChangesAsync();
+
+                result = this.RedirectToAction(nameof(this.Index), new { id = consultation.PatientId });
+            }
+
+            return result;
         }
     }
 }

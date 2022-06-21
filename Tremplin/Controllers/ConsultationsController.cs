aeed970b92ada092205c1e5deab0e91e7fff18ed
Antoine Siméon <a.simeon@proxiad.com>
2022-06-21 11:25:18 +0200

    [Feat] Add view for listing consultations

---
+++ Tremplin/Controllers/ConsultationsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tremplin.Data;
+using Tremplin.Models;
+
+namespace Tremplin.Controllers
+{
+    [Authorize]
+    public class ConsultationsController : Controller
+    {
+        private DataContext DataContext { get; init; }
+
+        public ConsultationsController(DataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+
+        /// <summary>
+        /// Provides access to the view for listing consultations
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Index(int patientId)
+        {
+            IQueryable<Consultation> consultations = from m in DataContext.Consultations
+                                                     where m.PatientId == patientId
+                                                     select m;
+
+            ConsultationListViewModel consultationListViewModel = new ConsultationListViewModel
+            {
+                Consultations = await consultations.ToListAsync()
+            };
+
+            return View(consultationListViewModel);
+        }
+    }
+}
---
+++ Tremplin/Models/ConsultationListViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Tremplin.Data;
+
+namespace Tremplin.Models
+{
+    public class ConsultationListViewModel
+    {
+        /// <summary>
+        /// Consultation Id
+        /// </summary>
+        [Key, Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Consultation date
+        /// </summary>
+        [Required]
+        [DisplayName("Date de consultation")]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Short description of the consultation
+        /// </summary>
+        [Required]
+        [DisplayName("Description")]
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// Long description of the consultation
+        /// </summary>
+        public string LongDescription { get; set; }
+
+        /// <summary>
+        /// Patient Id associated with this consultation
+        /// </summary>
+        [Required]
+        public int PatientId { get; set; }
+
+        /// <summary>
+        /// Patient associated with this consultation
+        /// </summary>
+        public Patient Patient { get; set; }
+
+        public List<Consultation>? Consultations { get; set; }
+    }
+}

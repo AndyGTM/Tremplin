3da17fc4286d7958bd01a695d6b51143aa1cf119
Antoine Siméon <a.simeon@proxiad.com>
2022-06-23 11:22:48 +0200

    [Build] Add "EntityFrameworkCore.DataEncryption" NuGet package

--- Tremplin/Data/DataContext.cs
+++ Tremplin/Data/DataContext.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.DataEncryption;
+using Microsoft.EntityFrameworkCore.DataEncryption.Providers;
 
 namespace Tremplin.Data
 {
     public class DataContext : DbContext
     {
+        // Get key and IV from a Base64String or any other ways
+        private readonly byte[] _encryptionKey = AesProvider.GenerateKey(AesKeySize.AES256Bits).Key;
+        private readonly byte[] _encryptionIV = AesProvider.GenerateKey(AesKeySize.AES256Bits).IV;
+        private readonly IEncryptionProvider _provider;
+
         public DataContext(DbContextOptions<DataContext> options)
             : base(options)
         {
+            this._provider = new AesProvider(this._encryptionKey, this._encryptionIV);
         }
 
         public DbSet<Patient> Patients { get; set; }
@@ -18,5 +26,10 @@ namespace Tremplin.Data
         public DbSet<User> Users { get; set; }
 
         public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseEncryption(this._provider);
+        }
     }
 }

cbe73f2242fda368cd5829abab99bedf4cc23898
AndyRamen <antoine_352@hotmail.fr>
2023-03-16 22:25:16 +0100

    [Test] Improve testing of comparison between birth date and today's date

--- Tremplin.Tests/CustomData/ComparisonDatesDataSourceAttribute.cs
+++ Tremplin.Tests/CustomData/ComparisonDatesDataSourceAttribute.cs
@@ -2,33 +2,21 @@
 
 namespace Tremplin.Tests.CustomData
 {
+    [AttributeUsage(AttributeTargets.Method)]
     public class ComparisonDatesDataSourceAttribute : Attribute, ITestDataSource
     {
-        /// <summary>
-        /// Collection of objects with dates, integers and strings used for testing the "DateTime.Compare()" method
-        /// </summary>
         public IEnumerable<object[]> GetData(MethodInfo methodInfo)
         {
-            // Earlier date than today (integer value is then equal to -1 when using "DateTime.Compare()")
-            yield return new object[] { new DateTime(1995, 04, 30), -1, "Birth date is earlier than today" };
+            yield return new object[] { "Birth date is earlier than today", new DateTime(1995, 04, 30), -1 };
 
-            // Same date as today (integer value is then equal to 0 when using "DateTime.Compare()")
-            yield return new object[] { DateTime.Today, 0, "Birth date is today" };
+            yield return new object[] { "Birth date is today", DateTime.Today, 0 };
 
-            // Later date than today (integer value is then equal to 1 when using "DateTime.Compare()")
-            yield return new object[] { new DateTime(3095, 04, 30), 1, "Birth date is later than today" };
+            yield return new object[] { "Birth date is later than today", new DateTime(3095, 04, 30), 1 };
         }
 
-        /// <summary>
-        /// Display names of data rows for comparison dates in the Test Explorer
-        /// </summary>
-        /// <param name="data">Object with date, integer and string, corresponding to a row for comparison dates in the Test Explorer</param>
-        public string GetDisplayName(MethodInfo methodInfo, object[] data)
+        public string? GetDisplayName(MethodInfo methodInfo, object?[]? data)
         {
-            if (data != null)
-                return string.Format("{0} ({1})", data[2], string.Format("{0:MM/dd/yyyy}", data[0]));
-
-            return null;
+            return data != null ? string.Format("{0} ({1})", data[0], string.Format("{0:MM/dd/yyyy}", data[1])) : null;
         }
     }
 }
--- Tremplin.Tests/PatientTest.cs
+++ Tremplin.Tests/PatientTest.cs
@@ -6,51 +6,32 @@ namespace Tremplin.Tests
     [TestClass]
     public class PatientTest
     {
-        /// <summary>
-        /// Comparison test between birth date and today's date
-        /// </summary>
-        /// <param name="comparisonExpected">Return -1 if BirthDate is earlier than Today, 1 if BirthDate is later than Today and 0 if the two dates are equal</param>
         [DataTestMethod]
-        // Arrange
         [ComparisonDatesDataSource]
-        public void Compare_BirthDate_WithToday(DateTime birthDate, int comparisonExpected, string displayName)
+        public void Compare_BirthDate_WithToday(string displayTestRowName, DateTime birthDate, int expectedComparison)
         {
-            // Act
-            int result = DateTime.Compare(birthDate, DateTime.Today);
+            int currentComparison = DateTime.Compare(birthDate, DateTime.Today);
 
-            // Assert
-            Assert.AreEqual(comparisonExpected, result);
+            Assert.AreEqual(expectedComparison, currentComparison);
         }
 
-        /// <summary>
-        /// Test if blank spaces in social security number are correctly added
-        /// </summary>
         [TestMethod("Add blank spaces in social security number")]
         public void Format_SocialSecurityNumber_AddBlankSpaces()
         {
-            // Arrange
             string socialSecurityNumber = "289113356951934";
 
-            // Act
             string result = SocialSecurityNumberHelper.AddBlankSpacesInSocialSecurityNumber(socialSecurityNumber);
 
-            // Assert
             Assert.AreEqual("2 89 11 33 569 519 34", result);
         }
 
-        /// <summary>
-        /// Test if blank spaces in social security number are correctly removed
-        /// </summary>
         [TestMethod("Remove blank spaces in social security number")]
         public void Format_SocialSecurityNumber_RemoveBlankSpaces()
         {
-            // Arrange
             string socialSecurityNumber = "1 90 05 35 777 888 55";
 
-            // Act
             string result = SocialSecurityNumberHelper.RemoveBlankSpacesInSocialSecurityNumber(socialSecurityNumber);
 
-            // Assert
             Assert.AreEqual("190053577788855", result);
         }
     }

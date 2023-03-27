918f4b29f122c25add8ad1def99e834310cd0ba1
AndyGTM <simeon.ant@gmail.com>
2023-03-28 00:56:09 +0200

    [Style] Remove comments in "Program.cs"

--- Tremplin.Web/Program.cs
+++ Tremplin.Web/Program.cs
@@ -10,29 +10,18 @@ using Tremplin.Store;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddIdentity<User, UserRole>().AddDefaultTokenProviders();
 
-// Consultation repository
 builder.Services.AddScoped(typeof(IConsultationRepository<Consultation>), typeof(ConsultationRepository<Consultation>));
-
-// Patient repository
 builder.Services.AddScoped(typeof(IPatientRepository<Patient>), typeof(PatientRepository<Patient>));
-
-// User repository
 builder.Services.AddScoped(typeof(IUserRepository<User>), typeof(UserRepository<User>));
 
-// Consultation service
 builder.Services.AddScoped(typeof(IConsultationService), typeof(ConsultationService));
-
-// Patient service
 builder.Services.AddScoped(typeof(IPatientService), typeof(PatientService));
-
-// User service
 builder.Services.AddScoped(typeof(IUserService), typeof(UserService));
 
 builder.Services.AddTransient<IUserStore<User>, UserStore>();
@@ -40,8 +29,6 @@ builder.Services.AddTransient<IRoleStore<UserRole>, RoleStore>();
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
-    // Password settings.
-
 #if DEBUG
     options.Password.RequireDigit = false;
     options.Password.RequireLowercase = false;
@@ -60,12 +47,10 @@ builder.Services.Configure<IdentityOptions>(options =>
 
 #endif
 
-    // Lockout settings.
     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
     options.Lockout.MaxFailedAccessAttempts = 5;
     options.Lockout.AllowedForNewUsers = true;
 
-    // User settings.
     options.User.AllowedUserNameCharacters =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
     options.User.RequireUniqueEmail = false;
@@ -90,12 +75,10 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 

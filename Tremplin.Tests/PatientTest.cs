094a02be0acba07aa63ffe59ac97efebe441313e
AndyRamen <antoine_352@hotmail.fr>
2023-04-01 19:57:59 +0200

    [Test] Add test for "GetPatientById" service

--- Tremplin.Tests/PatientTest.cs
+++ Tremplin.Tests/PatientTest.cs
@@ -1,4 +1,8 @@
+using Moq;
+using Tremplin.Core.Enums;
 using Tremplin.Core.Helpers;
+using Tremplin.IServices;
+using Tremplin.Models;
 using Tremplin.Tests.CustomData;
 
 namespace Tremplin.Tests
@@ -6,6 +10,29 @@ namespace Tremplin.Tests
     [TestClass]
     public class PatientTest
     {
+        [TestMethod("Get a patient by his id")]
+        public void Get_Patient_ById()
+        {
+            PatientModel patientModelMock = new()
+            {
+                Id = 20,
+                SocialSecurityNumber = "175043577788855",
+                LastName = "Julio",
+                FirstName = "Fabrice",
+                BirthDate = new DateTime(1975, 04, 30),
+                BloodGroup = BloodGroupNames.APositive,
+                Sex = SexTypes.Male,
+                SharedSheetWithOthersPractitioners = true,
+                CreatedBy = "Antoine"
+            };
+            Mock<IPatientService> patientServiceMock = new();
+            patientServiceMock.Setup(p => p.GetPatientById(patientModelMock.Id)).Returns(patientModelMock);
+
+            PatientModel patientModelResult = patientServiceMock.Object.GetPatientById(patientModelMock.Id);
+
+            Assert.AreEqual(patientModelMock, patientModelResult);
+        }
+
         [DataTestMethod]
         [ComparisonDatesDataSource]
         public void Compare_BirthDate_WithToday(string displayTestRowName, DateTime birthDate, int expectedComparison)
